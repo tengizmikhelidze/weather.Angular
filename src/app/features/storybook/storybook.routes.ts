a54bd64d0c4c9405936786da335a01f185dec752
10GO <mixelidzet@gmail.com>
2026-02-16 14:39:13 +0400

    button storybook

--- src/app/features/storybook/storybook.routes.ts
+++ src/app/features/storybook/storybook.routes.ts
@@ -1,17 +1,24 @@
 import {Routes} from '@angular/router';
+import {Storybook} from "./storybook";
 
 export const storybookRoutes: Routes = [
     {
         path: '',
-        redirectTo: 'menu',
-        pathMatch: 'full',
-    },
-    {
-        path: 'menu',
-        loadComponent: () => import('./menu-storybook/menu-storybook').then(m => m.MenuStorybook),
-    },
-    {
-        path: 'buttons',
-        loadComponent: () => import('./buttons-storybook/buttons-storybook').then(m => m.ButtonsStorybook),
+        component: Storybook,
+        children: [
+            {
+                path: "",
+                pathMatch: 'full',
+                redirectTo: 'buttons'
+            },
+            {
+                path: 'menu',
+                loadComponent: () => import('./menu-storybook/menu-storybook').then(m => m.MenuStorybook),
+            },
+            {
+                path: 'buttons',
+                loadComponent: () => import('./buttons-storybook/buttons-storybook').then(m => m.ButtonsStorybook),
+            },
+        ]
     },
 ];
--- src/app/features/storybook/storybook.ts
+++ src/app/features/storybook/storybook.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-storybook',
-  imports: [],
+    imports: [
+        RouterOutlet
+    ],
   templateUrl: './storybook.html',
   styleUrl: './storybook.scss',
 })
