ce5a1ce3f5043e4ab2473b748b4d660ddfb7f7dc
10GO <mixelidzet@gmail.com>
2026-02-16 14:19:51 +0400

    button styles

---
+++ src/app/features/storybook/buttons-storybook/buttons-storybook.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {ButtonComponent} from "../../../shared/components/buttons";
+
+@Component({
+  selector: 'app-buttons-storybook',
+    imports: [
+        ButtonComponent
+    ],
+  templateUrl: './buttons-storybook.html',
+  styleUrl: './buttons-storybook.scss',
+})
+export class ButtonsStorybook {
+
+}
